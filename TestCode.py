b29490dbf526deca0189c5dbb6db522ed3d95f15
MajorDuck <annguyen3132@gmail.com>
2023-02-24 09:34:46 -0600

    Change TestCode.py Output to Show Grid Position

--- TestCode.py
+++ TestCode.py
@@ -26,10 +26,6 @@ for _ in range(100):
     action = env.action_space.sample()  # agent policy that uses the observation and info
     observation, reward, terminated, truncated, info = env.step(action)
 
-# diplays the obeservation in a picture
-plt.imshow(observation)
-plt.show()
-
 # Define the height and width of the screen and the lower limit of the screen we want to search
 screen_height, screen_width, _ = env.observation_space.shape
 
@@ -37,7 +33,8 @@ screen_height, screen_width, _ = env.observation_space.shape
 # Define the color threshold for the red pixels
 red_threshold = 150
 
-# Loop through the bottom half of the screen and find the red pixels
+# Loop through the bottom half of the screen and
+# find the red pixels in the ball area
 red_pixels = []
 for x in range(startX, endX):
     for y in range(startY, endY):
@@ -46,13 +43,27 @@ for x in range(startX, endX):
             red_pixels.append((x, y))
 
 # Calculate grid position of ball
-ballX = int((red_pixels[0][0] - startX) / cellWidth)
-ballY = int((red_pixels[0][1] - startY) / cellHeight)
+if (len(red_pixels) > 0):
+    ballX = int((red_pixels[0][0] - startX) / cellWidth)
+    ballY = int((red_pixels[0][1] - startY) / cellHeight)
+
+# Loop through the bottom half of the screen and
+# find the red pixels in the paddle area
+red_pixels = []
+for x in range(startX, endX):
+    for y in range(endY, screen_height):
+        pixel_color = observation[y][x][:]
+        if pixel_color[0] > red_threshold:
+            red_pixels.append((x, y))
+
+# Calculate grid position of ball
+if (len(red_pixels) > 0):
+    paddleX = int((red_pixels[0][0] - startX) / cellWidth)
 
 print("Ball is in grid position: (", ballX, ", ", ballY, ")")
+print("Paddle is in position ",paddleX)
 
 # Print the coordinates of the red pixels
-print(red_pixels)
 plt.imshow(observation)
 plt.show()
 
