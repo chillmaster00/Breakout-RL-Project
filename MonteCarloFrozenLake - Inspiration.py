a7d47089ef37963beff7ee8a7e0e1303aeb93725
Trevor Maxwell <trevormaxwell00@gmail.com>
2023-03-04 17:39:16 -0600

    Fix MC and make it work with Frozen Lake

---
+++ MonteCarloFrozenLake - Inspiration.py
@@ -0,0 +1,44 @@
+import gymnasium as gym
+from gym import wrappers
+import numpy as np
+from tqdm import tqdm
+
+env = gym.make("FrozenLake-v1", is_slippery=False)
+
+Q = np.zeros([env.observation_space.n, env.action_space.n])
+n_s_a = np.zeros([env.observation_space.n, env.action_space.n])
+
+num_episodes = 100000
+epsilon = 0.2
+rList = []
+
+for i in tqdm(range(num_episodes)):
+    state, info = env.reset()
+    rAll = 0
+    terminated = False
+    truncated = False
+    results_list = []
+    result_sum = 0.0
+    while not (terminated or truncated):
+        if np.random.rand() < epsilon:
+            action = env.action_space.sample()
+        else:
+            action = np.argmax(Q[state, :])
+        new_state, reward, terminated, truncated, _ = env.step(action)
+        results_list.append((state, action))
+        result_sum += reward
+        state = new_state
+        rAll += reward
+    rList.append(rAll)
+
+    for (state, action) in results_list:
+        n_s_a[state, action] += 1.0
+        alpha = 1.0 / n_s_a[state, action]
+        Q[state, action] += alpha * (result_sum - Q[state, action])
+
+    if (i % 10000 == 0) and i != 0:
+        print("Success rate: " + str(sum(rList) / i))
+
+print("Success rate: " + str(sum(rList)/num_episodes))
+
+env.close()
--- TestCode.py
+++ TestCode.py
@@ -2,80 +2,11 @@ import gymnasium as gym
 from PIL import Image
 from matplotlib import pyplot as plt
 import numpy as np
-env = gym.make("ALE/Breakout-v5", render_mode="human")
+env = gym.make("FrozenLake-v1", render_mode="human")
 observation, info = env.reset()
 
-# Define a mapping of real state to abstract state
-# Here, we discretize the blank space between the
-# wall and the paddle as an 16 row by 18 column grid
-cellHeight = 6
-cellWidth = 8
-gridX = 18
-gridY = 16
-
-stateGrid = np.zeros((gridX, gridY))
-
-# Define start and end of discretization space
-startX = 8
-startY = 93
-endX = 152
-endY = 188
-
-
-for _ in range(100):
-    action = env.action_space.sample()  # agent policy that uses the observation and info
-    observation, reward, terminated, truncated, info = env.step(action)
-    print(info['lives'])
-
-# Define the height and width of the screen and the lower limit of the screen we want to search
-screen_height, screen_width, _ = env.observation_space.shape
-
-
-# Define the color threshold for the red pixels
-red_threshold = 150
-
-# Loop through the bottom half of the screen and
-# find the red pixels in the ball area
-red_pixels = []
-for x in range(startX, endX):
-    for y in range(startY, endY):
-        pixel_color = observation[y][x][:]
-        if pixel_color[0] > red_threshold:
-            red_pixels.append((x, y))
-
-# Calculate grid position of ball
-ballX, ballY = 0, 0
-if (len(red_pixels) > 0):
-    ballX = int((red_pixels[0][0] - startX) / cellWidth)
-    ballY = int((red_pixels[0][1] - startY) / cellHeight)
-
-# Loop through the bottom half of the screen and
-# find the red pixels in the paddle area
-red_pixels = []
-for x in range(startX, endX):
-    for y in range(endY, screen_height):
-        pixel_color = observation[y][x][:]
-        if pixel_color[0] > red_threshold:
-            red_pixels.append((x, y))
-
-# Calculate grid position of ball
-paddleX = 0
-if (len(red_pixels) > 0):
-    paddleX = int((red_pixels[0][0] - startX) / cellWidth)
-
-print("Ball is in grid position: (", ballX, ", ", ballY, ")")
-print("Paddle is in position ",paddleX)
-
-# Print the coordinates of the red pixels
-plt.imshow(observation)
-plt.show()
-
-print(reward)
-print(env.observation_space)
-print(env.action_space)
-
 for _ in range(1000):
-    action = 1  # agent policy that uses the observation and info
+    action = env.action_space.sample()  # agent policy that uses the observation and info
     observation, reward, terminated, truncated, info = env.step(action)
     if terminated or truncated:
         observation, info = env.reset()
